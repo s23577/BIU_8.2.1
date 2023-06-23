e4ece9e8fc19bece9c958e0e818b00ba85c7bdad
s23577 <73757032+s23577@users.noreply.github.com>
2023-06-23 19:27:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BIU_8.2.1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-kkbgff)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+import { Home, About, Events, Products, Contact } from './pages';
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
